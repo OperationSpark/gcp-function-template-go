799ad63e2609241e02d5bc698bf7a4618d8d32b6
Harvey Sanders <sanders.harvey@gmail.com>
2023-03-02 11:02:09 -0500

    Deployment dependent on CI success

--- function.go
+++ function.go
@@ -11,13 +11,13 @@ type (
 	}
 )
 
-// New creates an new App instance.
+// New creates a new App instance.
 func New() *App {
 	// Set up any dependencies here
 	return &App{}
 }
 
-// MainHandler is the entry point to the HTTP-triggered Cloud Function.
+// ServeHTTP is the entry point to the HTTP-triggered Cloud Function.
 func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
 }
